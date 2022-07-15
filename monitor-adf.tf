faaa48806a93862e6a4e6126149e1a5b2dc459b2
Mark Hahn <mhahn@tcbtech.com>
2022-07-14 19:18:30 -0700

    Enumerated all the possible log category flags.

--- monitor-adf.tf
+++ monitor-adf.tf
@@ -1,20 +1,7 @@
 # https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/monitor_diagnostic_setting
 
-resource "azurerm_monitor_diagnostic_setting" "edw_monitor_adf_ingestion_metrics" {
-  name                          = "edw-dev-diagnostics-adf-igestion-metrics"
-  target_resource_id            = azurerm_data_factory.edw_factory.id
-
-  log_analytics_workspace_id    = azurerm_log_analytics_workspace.edw_log_analytics_workspace.id
-  log_analytics_destination_type = "Dedicated" # or null see [documentation][1]
-
-  metric {
-    category                    = "AllMetrics"
-    enabled                     = true
-  }
-}
-
-resource "azurerm_monitor_diagnostic_setting" "edw_monitor_adf_ingestion_activties" {
-  name                          = "edw-dev-diagnostics-adf-igestion-activities"
+resource "azurerm_monitor_diagnostic_setting" "edw_monitor_adf_ingestion" {
+  name                          = "edw-dev-diagnostics-adf-igestion"
   target_resource_id            = azurerm_data_factory.edw_factory.id
 
   log_analytics_workspace_id    = azurerm_log_analytics_workspace.edw_log_analytics_workspace.id
@@ -25,4 +12,59 @@ resource "azurerm_monitor_diagnostic_setting" "edw_monitor_adf_ingestion_activti
     enabled                     = true
   }
 
+  log {
+    category                    = "PipelineRuns"
+    enabled                     = true
+  }
+
+  log {
+    category                    = "TriggerRuns"
+    enabled                     = true
+  }
+
+
+  log {
+    category                    = "SandboxActivityRuns"
+    enabled                     = true
+  }
+
+  log {
+    category                    = "SandboxPipelineRuns"
+    enabled                     = true
+  }
+
+  log {
+    category                    = "SSISPackageEventMessages"
+    enabled                     = true
+  }
+
+  log {
+    category                    = "SSISPackageExecutableStatistics"
+    enabled                     = true
+  }
+
+  log {
+    category                    = "SSISPackageEventMessageContext"
+    enabled                     = true
+  }
+
+  log {
+    category                    = "SSISPackageExecutionComponentPhases"
+    enabled                     = true
+  }
+
+  log {
+    category                    = "SSISPackageExecutionDataStatistics"
+    enabled                     = true
+  }
+
+  log {
+    category                    = "SSISIntegrationRuntimeLogs"
+    enabled                     = true
+  }
+
+  metric {
+    category                    = "AllMetrics"
+    enabled                     = true
+  }
 }
